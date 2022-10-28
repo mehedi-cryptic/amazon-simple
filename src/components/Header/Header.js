1656a7aba510fdd9226ec8b559e96d52f36a200c
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-10-29 01:03:13 +0600

    header image added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
