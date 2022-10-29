d72766f80814af0968b029dd738b597c927654b8
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-10-29 11:32:57 +0600

    navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,11 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="" />
+            <nav >
+                <a href="/shop">Shop</a>
+                <a href="review">Order Review</a>
+                <a href="manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
